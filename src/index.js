3fdac2061b2e6e618ca121be1f4eb38f51e5a193
sanghun <salr921@gmail.com>
2022-01-28 19:36:46 -0800

    finished server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import chalk from "chalk";
 import todoRouter from "./todo/todoRouter.js";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express(); // server
 const PORT = process.env.PORT ?? 8000;
@@ -23,11 +25,9 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://test:test@cluster0.wnmw2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log(chalk.greenBright("connected DB successfully"));
+    console.log(chalk.red("connected DB successfully"));
     app.listen(8000, function () {
       console.log(chalk.red("server is started successfully on 8000"));
     });
